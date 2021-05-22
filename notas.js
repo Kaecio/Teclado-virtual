a0bd3aa06b49809e39cbd48806e4a569e1536ff4
Kaecio <56613892+Kaecio@users.noreply.github.com>
2021-05-22 20:35:48 -0300

    removendo arquivo

--- notas.js
+++
@@ -1,9 +0,0 @@
-const cs2 = document.querySelector(".cs2");
-
-const a = cs2.addEventListener("mousedown", () => {
-  let audio = new Audio("audio/c2.mp3");
-  console.log("chama");
-  audio.play();
-});
-
-module.exports = { a };
